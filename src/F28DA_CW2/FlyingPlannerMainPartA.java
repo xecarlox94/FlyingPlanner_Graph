88e9e511ceef252a2e7882d96a0d5beddc6a0556
jcc8 <jcc8@hw.ac.uk>
2019-11-15 19:51:03 +0000

    continuing the planner A

--- src/F28DA_CW2/FlyingPlannerMainPartA.java
+++ src/F28DA_CW2/FlyingPlannerMainPartA.java
@@ -4,10 +4,9 @@ import java.io.FileNotFoundException;
 import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.Scanner;
 
 import org.jgrapht.Graph;
-import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleDirectedWeightedGraph;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
@@ -50,9 +49,11 @@ public class FlyingPlannerMainPartA {
     			// creates an airport using the string
     			Airport tempAirport = new Airport(airport[0], airport[2], airport[1]);
 
+    			
     			// adds the airport as a graph vertex
     			graph.addVertex(tempAirport);
 
+    			
     			// adds the same temporary airport to a hash table
     			airportHashTable.put(tempAirport.getCode(), tempAirport);
     		}
@@ -66,37 +67,108 @@ public class FlyingPlannerMainPartA {
     			// stores the temporary flight code string
     			String flightCode = flight[0];
 
+    			
     			// stores the temporary origin airport code string
     			String originAirportCode = flight[1];
 
+    			
     			// stores the temporary departure time
     			String fromTime = flight[2];
 
+    			
     			// stores the temporary origin airport code string
     			String destinationAirportCode = flight[3];
 
+    			
     			// stores the temporary arrival time
     			String toTime = flight[4];
+    			
     			
     			// it parses the temporary flight price string into an integer
     			int price = Integer.parseInt(flight[5]);
     			
+    			
     			// it retrieves the corresponding origin airport corresponding to the code
     			Airport originAirport = airportHashTable.get(originAirportCode);
 
+    			
     			// it retrieves the corresponding departure airport corresponding to the code
     			Airport destinationAirport = airportHashTable.get(destinationAirportCode);
+    			
     			
     			// it creates the new flight variable
     			Flight newFlight = new Flight(flightCode,fromTime,originAirport,toTime,destinationAirport,price);
 
+    			
     			// it adds the flight as a graph edge
     			graph.addEdge(originAirport, destinationAirport, newFlight);
     			
+    			
+    			// cast integer price to double
+    			double dPrice = (double) price;
+    			
+    			
+    			// set the double price as the weight of each edge
+    			graph.setEdgeWeight(newFlight, dPrice);
+    		}    		
+    		
+    		// printing the list of airports
+    		System.out.print("The following airports are used");
+    		
+    		
+    		// gets an iterator of the vertex set, for display purposes
+    		Iterator<Airport> airportSet = graph.vertexSet().iterator();
+    		
+    		
+    		// loops through the iterator, to display the names of the available airports
+    		while(airportSet.hasNext())
+    		{
+    			// stores the airport
+    			Airport tempAirport = airportSet.next();
+
+    			// prints the airport name
+//    			System.out.println(tempAirport.getLocation());
+    			
+    			if(tempAirport.getLocation().equals("Newcastle"))
+    			{
+        			// prints the airport name
+//        			System.out.println(tempAirport.getLocation());
+        			System.out.println(tempAirport.getName());
+    			}
     		}
+    		
+    		// to extract the string
+    		Scanner sc = new Scanner(System.in);
+    		
+    		// makes sure the airports are found, otherwise asks user again
+    		boolean airportsFound = false;
+    		
+    		while ( !airportsFound )
+    		{
+    			// questions the origin airport
+        		System.out.println("Please enter the start airport code \n");
+        		
+        		// stores the origin aiport location string
+        		String origin = sc.nextLine();
+        		
+        		
+        		
+        		System.out.println("Please enter the destination airport code \n");
+        		
+        		// stores the origin aiport loaction string
+        		String destination = sc.nextLine();
+    		}
+    		
+    		
+    		
+
+    		
+    		// it initialises the dijkstra algorithm, by passing the graph as a parameter
+    		DijkstraShortestPath<Airport,Flight> dijkstra = new DijkstraShortestPath<Airport,Flight>(graph);
+    		
             
-            
-		} catch (FileNotFoundException | FlyingPlannerException e) 
+		} 
+        catch (FileNotFoundException | FlyingPlannerException e) 
         {
 			System.err.println(e);
 		}
