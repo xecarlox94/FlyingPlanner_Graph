b89845fc56e6196cd6aeaf37da63fc51e96f7335
jcc8 <jcc8@HW-GRDL-PC104.hw.ac.uk>
2019-11-20 11:20:03 +0000

    Journey totalCost and totalHop finished

--- src/F28DA_CW2/Journey.java
+++ src/F28DA_CW2/Journey.java
@@ -25,23 +25,24 @@ public class Journey implements IJourneyPartB<Airport, Flight>, IJourneyPartC<Ai
 		// getting airport objects list from graph path 
 		List<Airport> airportList  = this.graphPath.getVertexList();
 		
-		// initialising airport string list
-		LinkedList<String> airportsStringList = new LinkedList<String>();
+		// initialising airport code string list
+		LinkedList<String> airportsCodeList = new LinkedList<String>();
 		
 		// iterating through the airport object list
 		for(int i = 0; i < airportList.size(); i++)
 		{
 			// storing the temporary airport variable
-			Airport tempFlight = airportList.get(i);
+			Airport tempAirport = airportList.get(i);
 			
 			// stores the airport code
-			String flightCode = tempFlight.getCode();
+			String airportCode = tempAirport.getCode();
 			
 			// adds the airport code 
-			airportsStringList.add(flightCode);
+			airportsCodeList.add(airportCode);
 		}
-				
-		return airportsStringList;
+		
+		// return the airport code list
+		return airportsCodeList;
 	}
 
 	@Override
@@ -51,7 +52,7 @@ public class Journey implements IJourneyPartB<Airport, Flight>, IJourneyPartC<Ai
 		List<Flight> flightsList  = this.graphPath.getEdgeList();
 		
 		// initialising flight string list
-		LinkedList<String> flightsStringList = new LinkedList<String>();
+		LinkedList<String> flightsCodeList = new LinkedList<String>();
 		
 		// iterating through the flight object list
 		for(int i = 0; i < flightsList.size(); i++)
@@ -63,23 +64,46 @@ public class Journey implements IJourneyPartB<Airport, Flight>, IJourneyPartC<Ai
 			String flightCode = tempFlight.getFlightCode();
 			
 			// adds the flight code 
-			flightsStringList.add(flightCode);
+			flightsCodeList.add(flightCode);
 		}
-				
-		return flightsStringList;
+
+		// return flight code string list
+		return flightsCodeList;
 	}
 
 	@Override
 	public int totalHop() 
 	{
-		// TODO Auto-generated method stub
-		return 0;
+		// gets the airports list (stops)
+		List<Airport> airports = this.graphPath.getVertexList();
+		
+		// number of airport hops
+		int numberHops = airports.size() - 1;
+		
+		return numberHops;
 	}
 
 	@Override
-	public int totalCost() {
-		// TODO Auto-generated method stub
-		return 0;
+	public int totalCost() 
+	{
+		// getting flight objects list from graph path 
+		List<Flight> flightsList = this.graphPath.getEdgeList();
+		
+		// initialises total cost accumulator
+		int totalCost = 0;
+		
+		// loops through each flight in list
+		for( int i = 0; i < flightsList.size(); i++ )
+		{
+			// stores the index flight
+			Flight tempFlight = flightsList.get(i);
+
+			// accumulates the fight cost into the total cost
+			totalCost += tempFlight.getCost();
+		}
+		
+		
+		return totalCost;
 	}
 
 	@Override
@@ -97,6 +121,12 @@ public class Journey implements IJourneyPartB<Airport, Flight>, IJourneyPartC<Ai
 	@Override
 	public int totalTime() {
 		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+	private int decTime(String stringTime) 
+	{
 		return 0;
 	}
 
