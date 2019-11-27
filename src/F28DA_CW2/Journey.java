a7e306529db419cfe08a2291ff1e4f59773a5117
jcc8 <jcc8@hw.ac.uk>
2019-11-27 19:36:17 +0000

    fixing and adding comments to journey class

--- src/F28DA_CW2/Journey.java
+++ src/F28DA_CW2/Journey.java
@@ -110,18 +110,22 @@ public class Journey implements IJourneyPartB<Airport, Flight>, IJourneyPartC<Ai
 		// getting flight objects list from graph path 
 		List<Flight> flightsList  = this.graphPath.getEdgeList();
 		
+		// initialises the total air time minutes to zero
 		int airTimeTotalMin = 0;
 		
+		// iterates through the flight set
 		for(int i = 0; i < flightsList.size(); i++)
 		{
+			// gets each flight
 			Flight tempFlight = flightsList.get(i);
 			
+			// gets its air time
 			int airTimeMin = this.getAirTimeMinFlight(tempFlight);
 			
+			// adds each air time to the total air time
 			airTimeTotalMin += airTimeMin;
 		}
 
-		
 		return airTimeTotalMin;
 	}
 
@@ -131,22 +135,29 @@ public class Journey implements IJourneyPartB<Airport, Flight>, IJourneyPartC<Ai
 		// getting flight objects list from graph path 
 		List<Flight> flightsList  = this.graphPath.getEdgeList();
 		
+		// stores the previous flight
 		Flight previousFlight;
 		
+		// stores the next flight
 		Flight nextFlight;
 		
+		// initialises the total connecting time between all the flights
+		// and assigns 0 minutes to it
 		int connectTimeTotalMin = 0;
 		
+		// iterates through the flight list
 		for(int i = 0; i < flightsList.size() - 1; i++)
 		{
+			// gets the current flight
 			previousFlight = flightsList.get(i);
 
+			// gets the following flight 
 			nextFlight = flightsList.get(i + 1);
 			
+			// gets the connecting time between the two flights
 			int tConnectTimeMin = this.getConnectionTimeMinFlights(previousFlight, nextFlight);
 			
-			previousFlight = nextFlight;
-			
+			// adds the connecting time to the total connecting time
 			connectTimeTotalMin += tConnectTimeMin;
 		}
 		
@@ -156,47 +167,73 @@ public class Journey implements IJourneyPartB<Airport, Flight>, IJourneyPartC<Ai
 	@Override
 	public int totalTime() 
 	{
+		// gets the journey total air time
 		int airTime = this.airTime();
 		
+		// gets the journey total connection time
 		int connectionTime = this.connectingTime();
 		
+		// adds the connection and the air time
+		// to calculate the total journey time
 		int totalTime = airTime + connectionTime;
 		
 		return totalTime;
 	}
 	
+	
+	/**
+	 * It gets the air time of each flight
+	 * */
 	private int getAirTimeMinFlight(Flight flight)
 	{
-		
+		// store the departure time string
 		String departureTime = flight.getFromGMTime();
 		
+		// stores the arrival time string
 		String arrivalTime = flight.getToGMTime();
 		
+		// gets the minutes resultant from the subtraction
+		// of arrival minus the departure time
 		int minutesAirTime = this.getMinutesSubStr(arrivalTime, departureTime);
 		
 		return minutesAirTime;
 	}
 	
-	
+	/**
+	 * It gets the connection time between two successive flights
+	 * */
 	private int getConnectionTimeMinFlights(Flight previousFlight, Flight nextFlight)
 	{
+		// gets the arrival time of the previous flight
 		String arrivalTime = previousFlight.getToGMTime();
 		
+		// gets the departure time of the next flight
 		String departureTime = nextFlight.getFromGMTime();
 
+		// gets the minutes resultant from the subtraction
+		// of next flight departure time minus the previous flight arrival
 		int minutesConnectionTime = this.getMinutesSubStr(departureTime, arrivalTime);
 		
 		return minutesConnectionTime;
 	}
 
+	/**
+	 * It subtracts two times in string format
+	 * */
 	private int getMinutesSubStr(String time1, String time2)
 	{
+		// gets the first string total decimal full time in float format
 		float decTime1 = Time.getDecFullTime(time1);
-		
+
+		// gets the second string total decimal full time in float format
 		float decTime2 = Time.getDecFullTime(time2);
 		
+		// subtracts two decimal float time values
+		// and stores its float decimal result in a variable
 		float decTotalTime = Time.subDecTimes(decTime1, decTime2);
 		
+		// gets the integer total minutes 
+		// resultant from the total decimal time in float format 
 		int totalMinutes = Time.getMinutes(decTotalTime);
 		
 		return totalMinutes;
